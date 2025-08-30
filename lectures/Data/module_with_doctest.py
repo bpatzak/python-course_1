93bab84a6559cd5c22bee622cf57eebe6319ce73
Borek Patzak <borek.patzak@gmail.com>
2025-08-30 13:19:48 +0200

    Adding new lectures

---
+++ lectures/Data/module_with_doctest.py
@@ -0,0 +1,15 @@
+def add(a, b):
+    """
+    Function to add two numbers.
+    Examples:
+    >>> add(2, 3)
+    5
+    >>> add(5, -2)
+    3
+    """
+    return a + b
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
+    
